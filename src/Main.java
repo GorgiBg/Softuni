fe1bd407dc555b4463da3da2f1faf32bf1a107c3
Georgi Djufov <gorgi82@abv.bg>
2020-10-29 10:41:31 +0200

    Hotel Reservation Project

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,20 @@
+
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println(5);
-ArrayList<Integer> numbers = new ArrayList<>();
+
+        String[] tokens = sc.nextLine().split("\\s+");
+        double pricePerDay =  Double.parseDouble(tokens[0]);
+        int days = Integer.parseInt(tokens[1]);
+        Season season = Season.valueOf(tokens[2]);
+        Discount discount = Discount.valueOf(tokens[3]);
+        System.out.printf("%.2f", PriceCalculator.calculatePrice(pricePerDay, days, season, discount));
+
+
     }
 }
 
