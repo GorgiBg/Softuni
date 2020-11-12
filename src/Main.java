68089d318e5a78e393472bf186c3a79bb207493a
Georgi Djufov <gorgi82@abv.bg>
2020-11-12 12:21:16 +0200

    Vehicles Project

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -6,22 +5,31 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        String[] tokens = sc.nextLine().split("\\s+");
-        double pricePerDay =  Double.parseDouble(tokens[0]);
-        int days = Integer.parseInt(tokens[1]);
-        Season season = Season.valueOf(tokens[2]);
-        Discount discount = Discount.valueOf(tokens[3]);
-        System.out.printf("%.2f", PriceCalculator.calculatePrice(pricePerDay, days, season, discount));
+        Map<String, Vehicle> vehicles = new HashMap<>();
+        String[] carInfo = sc.nextLine().split("\\s+");
+        vehicles.putIfAbsent("Car", new Car(Double.parseDouble(carInfo[1]), Double.parseDouble(carInfo[2])));
+        String[] truckInfo = sc.nextLine().split("\\s+");
+        vehicles.putIfAbsent("Truck", new Truck(Double.parseDouble(truckInfo[1]), Double.parseDouble(truckInfo[2])));
+        int n = Integer.parseInt(sc.nextLine());
+        while (n-->0){
+            String[] tokens = sc.nextLine().split("\\s+");
+            String command = tokens[0];
+            String type = tokens[1];
+            Double value = Double.parseDouble(tokens[2]);
+            switch (command){
+                case "Drive":
+                    System.out.println(vehicles.get(type).drive(value));
+                    break;
+                case "Refuel":
+                    vehicles.get(type).refuel(value);
+                    break;
+            }
+        }
+        for (Map.Entry<String, Vehicle> vehicle : vehicles.entrySet()) {
+            System.out.println(vehicle.getValue().toString());
+        }
 
 
     }
 }
-
-
-
-
-
-
-
 
